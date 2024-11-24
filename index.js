33b6bc5aa9ebe75d56e0369fd538d92d595ed486
softwarecompanycoltd <softwarecompanycoltd@gmail.com>
2024-11-24 21:41:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,5 +28,112 @@ function resigster () {
   favourite_song = document.getElementById('favourite_song').value
   milk_before_cereal = document.getElementById('milk_before_cereal').value
 
-  // validate input fiels 
+  // validate input fiels
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password Incorrect)
+    return
+    // Don't continue running the code
+  }
+  if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false || ) {
+    alert('Email or Password Incorrect)
+    return
+  }
+}
+
+// set up our login function
+function login () {
+  // get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+
+  // validate input fiels
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Email or Password Incorrect)
+    return
+    // Don't continue running the code
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+  .then(function() {
+    // Add this user to Firebase Database
+    var database_ref = database.ref()
+  
+    // Create User data
+    var user_data = {
+      last_login : Date.now()
+    }
+
+    database_ref.child('users/' + user.uid).set(user_data)
+    alert('User Logged Successfully!')
+    
+  })
+  .catsh(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    alert(error_message)
+  }
+}
+
+// Move on with Auth
+auth.createUserWithEmailAndPassword(email, password)
+.then(function() {
+  // Declare user variable
+  var user = auth.currentUser
+
+  // Add this user to Firebase Database
+  var database_ref = database.ref()
+  
+  // Create User data
+  var user_data = {
+    email : email,
+    full_name : full_name,
+    favourite_song : favourite_song,
+    milk_before_crereal : milk_before_crereal,
+    last_login : Date.now()
+  }
+
+  database_ref.child('users/' + user.uid).set(user_data)
+  alert('User Created Successfully!')
+})
+.catsh(function(error) {
+  // Firebase will use this to alert of its errors
+  var error_code = error.code
+  var error_message = error.message
+
+  alert(error_message)
+}
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // Email is Good
+    return true
+  } else {
+    // Email is not Good
+    return false
+  }
+}
+
+function validate_password(password) {
+  // firebase only accepts length greater than 6
+  if (password < 6) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function validate_field(field) {
+  if (field === null) {
+    return false
+  }
+
+  if (field.length <= 0) {
+    return false
+  } else {
+    return true
+  }
 }
